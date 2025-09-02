72356eeb0071739e6e593fe3627dbf4252a02f2d
Adam Pawlowski <apawlowski@simplito.com>
2025-09-02 16:57:10 +0200

    Resolve "autoryzacja w infra"

---
+++ server/dependecies.py
@@ -0,0 +1,16 @@
+"""Main app module."""
+
+from typing import Annotated
+
+from fastapi import Depends, HTTPException, Request
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+
+oauth2_scheme = HTTPBearer()
+
+
+def auth_server(request: Request, api_key: Annotated[HTTPAuthorizationCredentials, Depends(oauth2_scheme)]) -> str:
+    """Authenticate an server key."""
+    if api_key.credentials == request.app.state.context.config.api_key:
+        return api_key.credentials
+
+    raise HTTPException(status_code=401, detail="Unauthorized")
